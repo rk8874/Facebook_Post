fecfb6a9e2cdfc34731837c3b2fcb7a260fa6acf
rk8874 <107619897+rk8874@users.noreply.github.com>
2022-07-02 22:08:05 +0530

    Add files via upload

---
+++ faceImage.py
@@ -0,0 +1,26 @@
+import facebook
+import os
+import sys
+import io
+from PIL import Image
+import sys
+import argparse
+import json
+parser = argparse.ArgumentParser()
+parser.add_argument("-file_path", "--Input", help = "Show Output")
+parser.add_argument("-description", "--input", help = "Show Output")
+parser.add_argument("-json", "--Input1", help = "Show Output")
+args = parser.parse_args()
+f = open(args.Input1)
+data = json.load(f)
+jsonData = data["Facebook"]
+token= jsonData["Access_token"]
+page_Id= jsonData["Page_Id"]
+file=args.Input+jsonData["File_Name"]
+def main():
+    graph = facebook.GraphAPI(access_token=token, version='3.1')
+    response=graph.put_photo(image=open(file, 'rb'),caption=args.input, profile_id=page_Id)
+    print (f"Image_Id: {response['id']}")
+    print("Image uploaded!!")
+if __name__ == "__main__":
+    main()
